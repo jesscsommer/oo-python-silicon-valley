51988bb18529c75898eee39fca73d1e1439d3ee9
Jessica Sommerville <jessica.c.sommerville@gmail.com>
2023-05-29 16:52:40 -0700

    Build VC aggregate methods

--- lib/venture_capitalist.py
+++ lib/venture_capitalist.py
@@ -21,6 +21,42 @@ class VentureCapitalist:
     def tres_commas_club(cls):
         return [vc for vc in cls.all 
                 if vc.total_worth > 1000000000]
+    
+    # Associations & Aggregate Methods
+    
+    def offer_contract(self, startup, invest_type, amt): 
+        from .startup import Startup
+        if not isinstance(startup, Startup): 
+            raise TypeError("Startup must be an instance of Startup")
+        if type(invest_type) != str: 
+            raise TypeError("Investment type must be a string")
+        if type(amt) != float:
+            raise TypeError("Investment amount must be a float")
+        FundingRound(startup, self, invest_type, amt)
+    
+    def funding_rounds(self):
+        return [fr for fr in FundingRound.all if fr._venture_capitalist == self]
+
+    def portfolio(self):
+        if self.funding_rounds(): 
+            return {fr._startup for fr in self.funding_rounds()}
+        return "This VC has not funded any startups yet"
+    
+    def biggest_investment(self):
+        if self.funding_rounds():
+            largest = self.funding_rounds()[0]
+            for fr in self.funding_rounds():
+                if fr.investment > largest.investment: 
+                    largest = fr 
+            return largest 
+        return "No investments yet"
+    
+    def invested(self, domain):
+        results = [fr.investment for fr in self.funding_rounds() 
+                    if fr._startup._domain.lower() == domain.lower()]
+        if results: return sum(results)
+        return "Nothing invested in that domain"
+    
 
     # Properties
     @property
